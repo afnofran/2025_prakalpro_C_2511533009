890f9d50fc7e1375c0cbe2261720dd102ab3880b
AfifNaufalZahran <dawnzahran@gmail.com>
2025-09-17 18:24:26 +0700

    praktikum 1 kelas C

--- prakalpro_2025_c_2511533009/src/pekan2/tugasAlproPekan2.java
+++ prakalpro_2025_c_2511533009/src/pekan2/tugasAlproPekan2.java
@@ -1,45 +1,24 @@
 package pekan2;
-/* 
- * Import class Scanner dari package java.util
- * agar dapat melakukan input
- * 
- * */
-import java.util.Scanner;
 
 public class tugasAlproPekan2 {
 
+	/*
+	 * Program menampilkan data item game.
+	 * menampilkan Item Level, Harga Item, 
+	 * Kategori, apakah Tersedia
+	 */
 	public static void main(String[] args) {
-		/* Buat object scanner 
-		 * agar dapat memanggil method nextLine() dan nextInt() 
-		 * sehingga dapat melakukan input
-		 */
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(" === Data Mahasiswa ===");
+		int levelItem = 69; // Variabel Level Item dengan type data integer
+		float hargaItem = 67000.99f; // Variabel harga item di pasar dengan type data float
+		char kategoriItem = 'A'; // Variabel kategori item, bisa berupa A,B,C,D dengan type data char
+		boolean apakahTersedia = true; // Variabel ketersedian item apakah item tersedia? true/false dengan type data boolean
 		
-		// Menerima input NIM dan Nama dengan type data String
-		System.out.print("NIM	: ");
-		String nim = scanner.nextLine();
-		System.out.print("Nama	: ");		
-		String nama = scanner.nextLine();
-
-		// Menerima input Umur dengan type data Integer
-		System.out.print("Umur	: ");
-		int umur = scanner.nextInt();
+		// Menampilkan data item game
+		System.out.println("=== Data Item Game ===");
+		System.out.println("Level	: " + levelItem);
+		System.out.println("Harga	: " + hargaItem + " $");
+		System.out.println("Kategori: " + kategoriItem);
+		System.out.println("Tersedia: " + apakahTersedia);
 		
-		// Konversi type data NIM dari String menjadi Long
-		long longNIM = Long.parseLong(nim);
-		
-		// Lalu tambahkan NIM dengan Umur
-		longNIM += umur;
-		
-		// Konversi type data UMUR menjadi String
-		String stringUMUR = String.valueOf(umur);
-		
-		/*Tampilkan hasil konversi type data Umur 
-		 * dari Integer menajdi String 
-		 * dan penjumlahan NIM + Umur 
-		 * */
-		System.out.println("Umur (String) : " + stringUMUR);
-		System.out.println("NIM + Umur	: " + longNIM);
 	}
 }
